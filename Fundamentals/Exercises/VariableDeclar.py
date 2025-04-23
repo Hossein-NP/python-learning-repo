82b430d0bf38d75cf35649f29fa932ed3ccaf314
Hossein Noruzpur <143217596+Hossein-NP@users.noreply.github.com>
2025-04-23 11:48:04 -0700

    fundamental-EX1

---
+++ Fundamentals/Exercises/VariableDeclar.py
@@ -0,0 +1,13 @@
+"""
+  Suppose you are programming a simple game. You need to store the position of an object in the game.
+  This position consists of two variables x and y, which specify the horizontal and vertical dimensions of the object, respectively.
+  How can you define and assign values ​​to these two variables?
+
+"""
+cordinate_x = 10
+cordinate_y = 20
+
+print("Current position:", cordinate_x, cordinate_y)
+
+x, y = 20, 10
+print("Current Position:(", x, ",", y,")")
