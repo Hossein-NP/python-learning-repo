626c4041d29480d0aa2062a6ffac21241bca3fb7
Hossein Noruzpur <143217596+Hossein-NP@users.noreply.github.com>
2025-04-23 12:00:56 -0700

    fundamental-EX2

--- Fundamentals/Exercises/VariableDeclar.py
+++ Fundamentals/Exercises/VariableDeclar.py
@@ -1,3 +1,4 @@
+# Exercise 1
 """
   Suppose you are programming a simple game. You need to store the position of an object in the game.
   This position consists of two variables x and y, which specify the horizontal and vertical dimensions of the object, respectively.
@@ -11,3 +12,21 @@ print("Current position:", cordinate_x, cordinate_y)
 
 x, y = 20, 10
 print("Current Position:(", x, ",", y,")")
+
+
+
+# Exercise 2
+"""
+  Suppose two variables x and y are defined with values ​​5 and 10.
+
+  a) Using an expression, swap the values ​​of these two variables.
+
+  b) Without using auxiliary variables, raise the sum of x and y to the power of 2.
+
+"""
+x_data = 5
+y_data = 10
+print(f"x: {x_data}  y: {y_data}")
+x_data += 5
+y_data -= 5
+print(f"x: {x_data}  y: {y_data}")
