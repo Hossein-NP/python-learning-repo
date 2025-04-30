f27ac897335dcdf65323e7f00bc1ffe9e8a32b60
Hossein Noruzpur <143217596+Hossein-NP@users.noreply.github.com>
2025-04-30 13:22:24 -0700

    Update conditionalStatement.py

--- Fundamentals/Exercises/conditionalStatement.py
+++ Fundamentals/Exercises/conditionalStatement.py
@@ -5,7 +5,9 @@ print("*" *30, "Conditional Statement Exercise:", "*" *30, end="\n\n")
 """
   positive, negative or zero
 """
-number = int(input("Enter a number: "))
+print("*" *30, "Exercise 1:", "*" *30, end="\n\n")
+
+number = float(input("Enter a number: "))
 if number < 0:
   print(f"The entered number is negative.")
 
@@ -15,5 +17,30 @@ elif number > 0:
 else:
   print(f"The entered number is zero.")
 
-print(f"The entered number is {number}.")
+print(f"The entered number is {number}.", end="\n\n")
 
+# Exercise 1: try/except
+print("*" *30, "Exercise 1: try/except", "*" *30, end="\n\n")
+
+try:
+  number = float(input("Enter a number: "))
+  if number < 0:
+    print(f"The entered number is negative.")
+
+  elif number > 0:
+    print(f"The entered number is positive.")
+
+  else:
+    print(f"The entered number is zero.")
+
+  print(f"The entered number is {number}.", end="\n\n")
+
+except ValueError:
+  print("Invalid input! Please enter a numeric value.", end="\n\n")
+
+# Exercise 1: with while loop
+print("*" *30, "Exercise 1: with while loop", "*" *30, end="\n\n")
+
+while True:
+  number = float(input("Enter a number: "))
+  
