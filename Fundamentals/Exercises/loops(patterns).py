8ceb5c2297ac1a31cdfe30e7fb8af00f4a10908c
Hossein Noruzpur <143217596+Hossein-NP@users.noreply.github.com>
2025-05-05 15:08:10 -0700

    Update loops(patterns).py

--- Fundamentals/Exercises/loops(patterns).py
+++ Fundamentals/Exercises/loops(patterns).py
@@ -28,3 +28,33 @@ for i in range(1, 7):
   for j in range(1, i + 1):
     print("*", end="")
   print()
+
+print(end="\n\n")
+
+print("(*,*)" *5, "Loops >> PATTERNS: PATTERN 4: Complete pyramid:", "(*,*)" *5)
+
+k = 8
+step = 1
+for i in range(1, 7):
+  print(" " * (k - i), end="")
+  for j in range(1, i + step):
+    print("*", end="")
+  if i % 2 == 0:
+    step += 1
+  elif i % 2 == 1:
+    step += 1
+  print()
+
+print(end="\n\n")
+
+print("(*,*)" *5, "Loops >> PATTERNS: PATTERN 5: Inverted right triangle:", "(*,*)" *5)
+
+p = 8
+step_2 = 7
+for i in range(1, 7):
+  print(" " * (p - step_2), end="")
+  for j in range(1, p - i):
+    print("*", end="")
+  print()
+  step_2 -= 1
+  
