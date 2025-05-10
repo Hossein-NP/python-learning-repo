69925f35f8f50b732dc8a76b88688134518136f4
Hossein Noruzpur <143217596+Hossein-NP@users.noreply.github.com>
2025-05-10 06:55:34 -0700

    Update todo.py

--- Fundamentals/Exercises/todo.py
+++ Fundamentals/Exercises/todo.py
@@ -1,18 +1,31 @@
 # (-->ToDO APP)
-
 # (*.*)(*.*)(*.*)(*.*)(*.*)(*.*)
 
 # START PROGRAM
 
 works_list , works_time_list = [], []
-
 while True:
-    work = input("Enter what you want to do: ").lower()
+    # work input
+    work = input("Enter what you want to do (ex => walking, reading,.....): ").lower()
     works_list.append(work)
-    work_time = input("Enter the time of to do: ")
+    # work time input
+    while True:
+        work_time = input("Enter the time of to do (ex => 09 : 45): ").strip()
+        if ":" not in work_time:
+            print("Please enter the correct time format (e.g., 10:25)")
+        else:
+            break
     works_time_list.append(work_time)
-
     resume = input("Do you want to continue?(yes/no) ").lower()
     if resume == "no":
+        print()
         break
-print(f"Works list: {works_list}\nWorks List Time: {works_time_list}", end="\n\n")
+# show output
+print(f"Works List: {works_list}\nWork List Time: {works_time_list}", end="\n\n")
+# Save number of tasks and times
+tasks_number = len(works_list)
+tasks_time_number = len(works_time_list)
+# Table display of data
+print(" " * 3, "زمان انجام کار ها", " " * 3, "لیست کار ها", " " * 3, "شماره ردیف")
+for row in  range(1, tasks_number + 1):
+    
