93d1deb37ad8a3283f9536c1469b7b7716b1196f
Hossein Noruzpur <143217596+Hossein-NP@users.noreply.github.com>
2025-05-10 08:39:24 -0700

    Update todo.py

--- Fundamentals/Exercises/todo.py
+++ Fundamentals/Exercises/todo.py
@@ -16,9 +16,13 @@ while True:
         else:
             break
     works_time_list.append(work_time)
-    resume = input("Do you want to continue?(yes/no) ").lower()
+    while True:
+        resume = input("Do you want to continue?(yes/no) ").lower()
+        if resume != "no" and resume != "yes":
+            print("The answer is only yes or no")
+        elif resume == "yes" or resume == "no":
+            break
     if resume == "no":
-        print()
         break
 # show output
 print(f"Works List: {works_list}\nWork List Time: {works_time_list}", end="\n\n")
@@ -26,6 +30,21 @@ print(f"Works List: {works_list}\nWork List Time: {works_time_list}", end="\n\n"
 tasks_number = len(works_list)
 tasks_time_number = len(works_time_list)
 # Table display of data
-print(" " * 3, "زمان انجام کار ها", " " * 3, "لیست کار ها", " " * 3, "شماره ردیف")
-for row in  range(1, tasks_number + 1):
-    
+print("زمان انجام کار ها", " " * 3, " کار ها", " " * 3, "شماره ردیف")
+counter = 0
+for i in  range(0, tasks_number):
+    print("-" * 45)
+    print(i + 1, " " * 12, end=" ")
+    for j in range(1, 1 + 1):
+        print(works_list[counter], " " * 6, end=" ")
+        print(works_time_list[counter])
+    counter += 1
+# Clock code
+time = []
+for hour in range(0, 23 + 1):
+    for minute in range(1, 59 + 1):
+        # At this level of the program, I don't think we need seconds.
+        # for second in range(1, 59 + 1):
+        #print(f"{hour} : {minute}")
+        time.append(f"{hour} : {minute}")
+print(time)
