3852d01f7c3b705046f5cb720cfd115a750cb071
Hossein Noruzpur <143217596+Hossein-NP@users.noreply.github.com>
2025-08-03 11:37:53 -0700

    py --> numeric

--- Fundamentals/Exercises/Numeric.py
+++ Fundamentals/Exercises/Numeric.py
@@ -11,4 +11,23 @@ print(g, "g", sep="", end="\n\n")
 height = int(input("Please enter height: "))
 rule = int(input("Please enter rule: "))
 s = (1 / 2) * height * rule
-print("s = ", s)
+print("s = ", s, end="\n\n")
+
+# Calculator
+x = int(input("Enter x: "))
+y = int(input("Enter y: "))
+print(x, "+", y, "=", x + y)
+print(x, "-", y, "=", x - y)
+print(x, "*", y, "=", x * y)
+print(x, "/", y, "=", x / y)
+
+# Digit separator
+number  = int(input("please enter number: "))
+temp = number % 10  # In mathematics, the remainder of any number divided by 10 is the last digit of that number.
+print(temp)
+number = number // 10
+temp = number % 10
+print(temp)
+number = number // 10
+temp = number % 10
+print(temp)
